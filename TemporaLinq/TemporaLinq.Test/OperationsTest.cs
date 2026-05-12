6ee126067af8f07fde03961f6ffa77096d455f7a
Christopher Thonfeld-Guckes <christopher.thonfeld-guckes.ext@rohde-schwarz.com>
2026-05-12 17:47:24 +0200

    feat: add first operation

---
+++ TemporaLinq/TemporaLinq.Test/OperationsTest.cs
@@ -0,0 +1,43 @@
+﻿using de.baggerbagger.TemporaLinq;
+using FluentAssertions;
+
+namespace TemporaLinq.Test;
+
+public class OperationsTest
+{
+    [Fact]
+    public void MergingTwoStreams_YieldsAllDatesInTheRightOrder()
+    {
+        var first = Dates.Invariant().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 1, 3));
+        var second = Dates.Invariant().From(new DateOnly(2026, 1, 6)).To(new DateOnly(2026, 1, 8));
+
+        var merged = first.Merge(second);
+
+        merged.Should().ContainInOrder(
+            new DateOnly(2026, 1, 1), 
+            new DateOnly(2026, 1, 2), 
+            new DateOnly(2026, 1, 6),
+            new DateOnly(2026, 1, 7));
+    }
+
+    [Fact]
+    public void MergingThreeStreams_YieldsAllDatesInTheRightOrder()
+    {
+        var first = Dates.Invariant().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 1, 3));
+        var second = Dates.Invariant().From(new DateOnly(2026, 3, 1)).To(new DateOnly(2026, 3, 6));
+        var third = Dates.Invariant().From(new DateOnly(2026, 2, 1)).To(new DateOnly(2026, 2, 2));
+
+        var merged = Operations.Merge([first, second, third]);
+
+        merged.Should().ContainInOrder(
+            new DateOnly(2026, 1, 1), 
+            new DateOnly(2026, 1, 2), 
+            new DateOnly(2026, 2, 1),
+            new DateOnly(2026, 3, 1),
+            new DateOnly(2026, 3, 2),
+            new DateOnly(2026, 3, 3),
+            new DateOnly(2026, 3, 4),
+            new DateOnly(2026, 3, 5),
+            new DateOnly(2026, 3, 6));
+    }
+}
---
+++ TemporaLinq/TemporaLinq/Operations.cs
@@ -0,0 +1,56 @@
+﻿namespace de.baggerbagger.TemporaLinq;
+
+public static class Operations
+{
+    /// <summary>
+    /// Returns a stream of dates that contains all dates from the given streams in ascending order.
+    /// </summary>
+    /// <param name="dateStreams"></param>
+    /// <returns></returns>
+    public static IEnumerable<DateOnly> Merge(IEnumerable<IEnumerable<DateOnly>> dateStreams)
+    {
+        // iterate over all dateStreams and yield the lowest date, increment that enumerator then
+
+        var enumerators = dateStreams
+            .Select(stream => stream.GetEnumerator())
+            .Where(enumerator => enumerator.MoveNext())
+            .ToList();
+
+        var nextValues = new DateOnly[enumerators.Count];
+
+        for (var i = 0; i < enumerators.Count; i++)
+        {
+            nextValues[i] = enumerators[i].Current;
+        }
+
+        while (nextValues.Any(v => v != default))
+        {
+            var minValue = nextValues.Min();
+            if (minValue != DateOnly.MaxValue)
+                yield return minValue;
+            else
+                yield break;
+
+            for (var i = 0; i < nextValues.Length; i++)
+            {
+                if (nextValues[i] == minValue)
+                {
+                    var hasNext = enumerators[i].MoveNext();
+                    if (hasNext)
+                    {
+                        nextValues[i] = enumerators[i].Current;
+                    }
+                    else
+                    {
+                        nextValues[i] = DateOnly.MaxValue;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    public static IEnumerable<DateOnly> Merge(this IDateEnumerable dates, params IDateEnumerable[] others)
+        => Merge(new[] { dates }.Concat(others));
+}
