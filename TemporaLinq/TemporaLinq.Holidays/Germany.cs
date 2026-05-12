8cf6db9da30b8621087416223cbe2ed62908d605
Christopher Thonfeld-Guckes <christopher.thonfeld-guckes.ext@rohde-schwarz.com>
2026-05-12 09:07:46 +0200

    feat: add standard german state holidays

---
+++ TemporaLinq/TemporaLinq.Holidays/Germany.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+
+namespace TemporaLinq.Holidays.Christian;
+
+/// <summary>
+/// Provides German national holidays.
+/// </summary>
+public record GermanHolidays : IEnumerable<Holiday>
+{
+    public DateOnly StartDate { get; private init; }
+    public DateOnly EndDate { get; private init; }
+
+    private GermanHolidays()
+    {
+        StartDate = DateOnly.MinValue;
+        EndDate = DateOnly.MaxValue;
+    }
+
+    public static GermanHolidays Create() => new();
+
+    public GermanHolidays From(DateOnly date) => this with { StartDate = date };
+    public GermanHolidays To(DateOnly date) => this with { EndDate = date };
+
+    /// <summary>
+    /// Looks up a holiday for the specified date.
+    /// </summary>
+    /// <param name="date">The date to look up.</param>
+    /// <returns>The Holiday if found, otherwise null.</returns>
+    public Holiday? GetHoliday(DateOnly date)
+        => this.FirstOrDefault(h => h.Date == date) is { } holiday && holiday.Date == date
+            ? holiday
+            : null;
+
+    /// <summary>
+    /// Tries to get a holiday for the specified date.
+    /// </summary>
+    /// <param name="date">The date to look up.</param>
+    /// <param name="holiday">The holiday if found, otherwise default.</param>
+    /// <returns>True if a holiday was found, otherwise false.</returns>
+    public bool TryGetHoliday(DateOnly date, out Holiday? holiday)
+    {
+        holiday = GetHoliday(date);
+        return holiday is not null;
+    }
+    /// <summary>
+    /// Checks if the specified date is a holiday.
+    /// </summary>
+    /// <param name="date">The date to check.</param>
+    /// <returns>True if the date is a holiday, otherwise false.</returns>
+    public bool IsHoliday(DateOnly date)
+        => this.Any(h => h.Date == date);
+
+    public IEnumerator<Holiday> GetEnumerator()
+    {
+        for (var year = StartDate.Year; year <= EndDate.Year; year++)
+        {
+            foreach (var holiday in GetHolidaysForYear(year))
+            {
+                if (holiday.Date < StartDate)
+                    continue;
+                if (holiday.Date > EndDate)
+                    yield break;
+                yield return holiday;
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static IEnumerable<Holiday> GetHolidaysForYear(int year)
+    {
+        var easter = CalculateEasterSunday(year);
+        var ascensionDay = easter.AddDays(39);
+        var corpusChristi = easter.AddDays(60);
+        var whitMonday = easter.AddDays(50);
+        var mayFirst = new DateOnly(year, 5, 1);
+
+        yield return new Holiday(new DateOnly(year, 1, 1), "Neujahr");
+        yield return new Holiday(easter.AddDays(-2), "Karfreitag");
+        yield return new Holiday(easter.AddDays(1), "Ostermontag");
+
+        if (ascensionDay < mayFirst)
+        {
+            yield return new Holiday(ascensionDay, "Christi Himmelfahrt");
+            yield return new Holiday(mayFirst, "Tag der Arbeit");
+        }
+        else
+        {
+            yield return new Holiday(mayFirst, "Tag der Arbeit");
+            yield return new Holiday(ascensionDay, "Christi Himmelfahrt");
+        }
+
+        yield return new Holiday(whitMonday, "Pfingstmontag");
+        yield return new Holiday(corpusChristi, "Fronleichnam");
+
+        yield return new Holiday(new DateOnly(year, 10, 3), "Tag der Deutschen Einheit");
+        yield return new Holiday(new DateOnly(year, 12, 25), "Erster Weihnachtsfeiertag");
+        yield return new Holiday(new DateOnly(year, 12, 26), "Zweiter Weihnachtsfeiertag");
+    }
+
+    /// <summary>
+    /// Calculates Easter Sunday for a given year using the Gauss Easter formula (Ostersonntagsformel).
+    /// </summary>
+    public static DateOnly CalculateEasterSunday(int year)
+    {
+        var a = year % 19;
+        var b = year / 100;
+        var c = year % 100;
+        var d = b / 4;
+        var e = b % 4;
+        var f = (b + 8) / 25;
+        var g = (b - f + 1) / 3;
+        var h = (19 * a + b - d - g + 15) % 30;
+        var i = c / 4;
+        var k = c % 4;
+        var l = (32 + 2 * e + 2 * i - h - k) % 7;
+        var m = (a + 11 * h + 22 * l) / 451;
+
+        var month = (h + l - 7 * m + 114) / 31;
+        var day = ((h + l - 7 * m + 114) % 31) + 1;
+
+        return new DateOnly(year, month, day);
+    }
+}
+/// <summary>
+/// Represents a holiday with its date and name.
+/// </summary>
+public readonly record struct Holiday(DateOnly Date, string Name)
+{
+    public static implicit operator DateOnly(Holiday holiday) => holiday.Date;
+    public static implicit operator DateOnly?(Holiday? holiday) => holiday?.Date;
+}
+
+/// <summary>
+/// Provides access to German national holidays.
+/// </summary>
+public static class Germany
+{
+    public static GermanHolidays Holidays => GermanHolidays.Create();
+}
---
+++ TemporaLinq/TemporaLinq.Test/Holidays/GermanyTest.cs
@@ -0,0 +1,176 @@
+﻿using TemporaLinq.Holidays.Christian;
+using FluentAssertions;
+
+namespace TemporaLinq.Test.Holidays;
+
+public class GermanyTest
+{
+    [Fact]
+    public void GetHolidays_For2026_ReturnsAllHolidays()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        holidays.Should().HaveCount(10);
+    }
+
+    [Fact]
+    public void GetHolidays_AreOrderedChronologically()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31)).ToList();
+        
+        for (var i = 1; i < holidays.Count; i++)
+        {
+            holidays[i].Date.Should().BeAfter(holidays[i - 1].Date);
+        }
+    }
+
+    [Fact]
+    public void GetHolidays_ContainsFixedHolidays()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        holidays.Should().Contain(h => h.Date.Month == 1 && h.Date.Day == 1 && h.Name == "Neujahr");
+        holidays.Should().Contain(h => h.Date.Month == 5 && h.Date.Day == 1 && h.Name == "Tag der Arbeit");
+        holidays.Should().Contain(h => h.Date.Month == 10 && h.Date.Day == 3 && h.Name == "Tag der Deutschen Einheit");
+        holidays.Should().Contain(h => h.Date.Month == 12 && h.Date.Day == 25 && h.Name == "Erster Weihnachtsfeiertag");
+        holidays.Should().Contain(h => h.Date.Month == 12 && h.Date.Day == 26 && h.Name == "Zweiter Weihnachtsfeiertag");
+    }
+
+    [Fact]
+    public void GetHolidays_ContainsVariableHolidays()
+    {
+        var easter2026 = GermanHolidays.CalculateEasterSunday(2026);
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(-2) && h.Name == "Karfreitag");
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(1) && h.Name == "Ostermontag");
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(39) && h.Name == "Christi Himmelfahrt");
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(50) && h.Name == "Pfingstmontag");
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(60) && h.Name == "Fronleichnam");
+    }
+
+    [Theory]
+    [InlineData(2024, 3, 31)]
+    [InlineData(2025, 4, 20)]
+    [InlineData(2026, 4, 5)]
+    [InlineData(2027, 3, 28)]
+    [InlineData(2028, 4, 16)]
+    public void CalculateEasterSunday_ReturnsCorrectDate(int year, int expectedMonth, int expectedDay)
+    {
+        var easter = GermanHolidays.CalculateEasterSunday(year);
+        
+        easter.Year.Should().Be(year);
+        easter.Month.Should().Be(expectedMonth);
+        easter.Day.Should().Be(expectedDay);
+    }
+
+    [Fact]
+    public void GetHoliday_ReturnsHoliday_WhenDateIsHoliday()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        var newYears = holidays.GetHoliday(new DateOnly(2026, 1, 1));
+        
+        newYears.Should().NotBeNull();
+        newYears!.Value.Name.Should().Be("Neujahr");
+    }
+
+    [Fact]
+    public void GetHoliday_ReturnsNull_WhenDateIsNotHoliday()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        var result = holidays.GetHoliday(new DateOnly(2026, 1, 2));
+        
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void TryGetHoliday_ReturnsTrue_WhenDateIsHoliday()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        var success = holidays.TryGetHoliday(new DateOnly(2026, 1, 1), out var holiday);
+        
+        success.Should().BeTrue();
+        holiday!.Value.Name.Should().Be("Neujahr");
+    }
+
+    [Fact]
+    public void TryGetHoliday_ReturnsFalse_WhenDateIsNotHoliday()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        var success = holidays.TryGetHoliday(new DateOnly(2026, 1, 2), out var holiday);
+        
+        success.Should().BeFalse();
+        holiday.Should().BeNull();
+    }
+
+    [Fact]
+    public void IsHoliday_ReturnsTrue_WhenDateIsHoliday()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        holidays.IsHoliday(new DateOnly(2026, 1, 1)).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsHoliday_ReturnsFalse_WhenDateIsNotHoliday()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        holidays.IsHoliday(new DateOnly(2026, 1, 2)).Should().BeFalse();
+    }
+
+    [Fact]
+    public void DateRange_FiltersHolidaysCorrectly()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 6, 1)).To(new DateOnly(2026, 6, 30));
+        
+        holidays.Should().HaveCount(1); // Fronleichnam in June 2026
+    }
+
+    [Fact]
+    public void MultipleYears_ReturnsAllHolidays()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2025, 1, 1)).To(new DateOnly(2026, 12, 31));
+        
+        holidays.Should().HaveCount(20); // 10 holidays per year
+    }
+
+    [Fact]
+    public void Holiday_ImplicitlyConvertsToDateOnly()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+        var holiday = holidays.GetHoliday(new DateOnly(2026, 1, 1))!;
+        
+        DateOnly? date = holiday;
+        date.Should().Be(new DateOnly(2026, 1, 1));
+    }
+
+    [Fact]
+    public void AscensionDay_BeforeMayFirst_WhenEasterIsLate()
+    {
+        // When Easter is late (e.g., mid-April), Ascension Day (39 days after Easter)
+        // can fall after May 1
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31)).ToList();
+        
+        var ascensionDay = holidays.First(h => h.Name == "Christi Himmelfahrt");
+        var mayFirst = holidays.First(h => h.Name == "Tag der Arbeit");
+        
+        // In 2026, Easter is April 5, Ascension is May 14, so May 1 comes first
+        mayFirst.Date.Should().BeBefore(ascensionDay.Date);
+    }
+
+    [Fact]
+    public void WhitMonday_AlwaysBeforeCorpusChristi()
+    {
+        var holidays = Germany.Holidays.From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31)).ToList();
+        
+        var whitMonday = holidays.First(h => h.Name == "Pfingstmontag");
+        var corpusChristi = holidays.First(h => h.Name == "Fronleichnam");
+        
+        whitMonday.Date.Should().BeBefore(corpusChristi.Date);
+    }
+}
