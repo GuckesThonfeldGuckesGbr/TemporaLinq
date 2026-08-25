6ea1441dfdd04c24e3f0165fbbe7e72a58d566d9
Christopher Thonfeld-Guckes <ctg@baggerbagger.de>
2026-08-25 11:05:01 +0200

    feat: add Switzerland national holidays

---
+++ TemporaLinq/TemporaLinq.Holidays/Europe/Switzerland/NationalHolidays.cs
@@ -0,0 +1,35 @@
+using System.Collections.Immutable;
+using Memoizer;
+using static TemporaLinq.Holidays.HolidayNames;
+
+namespace TemporaLinq.Holidays.Europe.Switzerland;
+
+/// <summary>
+/// Provides the Swiss public holidays observed in the large majority of cantons.
+/// Full per-canton coverage is out of scope; see the type-level remarks.
+/// </summary>
+public record NationalHolidays : HolidayEnumerable<NationalHolidays>
+{
+    protected override IEnumerable<Holiday> GetHolidaysForYear(int year)
+        => GetHolidaysFor(year);
+
+    [Cache]
+    private static ImmutableList<Holiday> GetHolidaysFor(int year)
+    {
+        var easter = EasterSundayCalculation.Christian.ForYear(year);
+
+        return new List<Holiday>
+            {
+                new(new DateOnly(year, 1, 1), NewYearsDay),
+                new(easter.AddDays(-2), GoodFriday),
+                new(easter.AddDays(1), EasterMonday),
+                new(easter.AddDays(39), AscensionDay),
+                new(easter.AddDays(50), WhitMonday),
+                new(new DateOnly(year, 8, 1), NationalDayOfSwitzerland),
+                new(new DateOnly(year, 12, 25), ChristmasDay),
+                new(new DateOnly(year, 12, 26), StStephensDay),
+            }
+            .Order()
+            .ToImmutableList();
+    }
+}
---
+++ TemporaLinq/TemporaLinq.Test/Holidays/Europe/SwitzerlandTest.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using TemporaLinq.Holidays;
+using TemporaLinq.Holidays.Europe.Switzerland;
+using static TemporaLinq.Holidays.HolidayNames;
+
+namespace TemporaLinq.Test.Holidays.Europe;
+
+public class SwitzerlandTest
+{
+    [Fact]
+    public void GetHolidays_For2026_ReturnsAllHolidays()
+    {
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+
+        holidays.Should().HaveCount(8);
+    }
+
+    [Fact]
+    public void GetHolidays_ContainsFixedHolidays()
+    {
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 1, 1) && h.Name == NewYearsDay);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 8, 1) && h.Name == NationalDayOfSwitzerland);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 12, 25) && h.Name == ChristmasDay);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 12, 26) && h.Name == StStephensDay);
+    }
+
+    [Fact]
+    public void GetHolidays_ContainsVariableHolidays()
+    {
+        var easter2026 = EasterSundayCalculation.Christian.ForYear(2026);
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(-2) && h.Name == GoodFriday);
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(1) && h.Name == EasterMonday);
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(39) && h.Name == AscensionDay);
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(50) && h.Name == WhitMonday);
+    }
+}
