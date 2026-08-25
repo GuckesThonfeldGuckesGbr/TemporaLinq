e9e22b331bcbe3c82c76c88e98f4d6ec01171bec
Christopher Thonfeld-Guckes <ctg@baggerbagger.de>
2026-08-25 11:03:10 +0200

    feat: add Sweden national holidays

---
+++ TemporaLinq/TemporaLinq.Holidays/Europe/Sweden/NationalHolidays.cs
@@ -0,0 +1,41 @@
+using System.Collections.Immutable;
+using Memoizer;
+using static TemporaLinq.Holidays.HolidayNames;
+
+namespace TemporaLinq.Holidays.Europe.Sweden;
+
+using TemporaLinq.Dates;
+using Dates = TemporaLinq.Dates.Dates;
+
+/// <summary>
+/// Provides Swedish national public holidays.
+/// </summary>
+public record NationalHolidays : HolidayEnumerable<NationalHolidays>
+{
+    protected override IEnumerable<Holiday> GetHolidaysForYear(int year)
+        => GetHolidaysFor(year);
+
+    [Cache]
+    private static ImmutableList<Holiday> GetHolidaysFor(int year)
+    {
+        var easter = EasterSundayCalculation.Christian.ForYear(year);
+
+        return new List<Holiday>
+            {
+                new(new DateOnly(year, 1, 1), NewYearsDay),
+                new(new DateOnly(year, 1, 6), Epiphany),
+                new(easter.AddDays(-2), GoodFriday),
+                new(easter, EasterSunday),
+                new(easter.AddDays(1), EasterMonday),
+                new(new DateOnly(year, 5, 1), LabourDay),
+                new(easter.AddDays(39), AscensionDay),
+                new(new DateOnly(year, 6, 6), NationalDayOfSweden),
+                new(Dates.Invariant().From(new DateOnly(year, 6, 20)).First(DayOfWeek.Saturday), MidsummerDay),
+                new(Dates.Invariant().From(new DateOnly(year, 10, 31)).First(DayOfWeek.Saturday), AllSaintsDay),
+                new(new DateOnly(year, 12, 25), ChristmasDay),
+                new(new DateOnly(year, 12, 26), StStephensDay),
+            }
+            .Order()
+            .ToImmutableList();
+    }
+}
---
+++ TemporaLinq/TemporaLinq.Test/Holidays/Europe/SwedenTest.cs
@@ -0,0 +1,66 @@
+using FluentAssertions;
+using TemporaLinq.Holidays;
+using TemporaLinq.Holidays.Europe.Sweden;
+using static TemporaLinq.Holidays.HolidayNames;
+
+namespace TemporaLinq.Test.Holidays.Europe;
+
+public class SwedenTest
+{
+    [Fact]
+    public void GetHolidays_For2026_ReturnsAllHolidays()
+    {
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+
+        holidays.Should().HaveCount(12);
+    }
+
+    [Fact]
+    public void GetHolidays_ContainsFixedHolidays()
+    {
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 1, 1) && h.Name == NewYearsDay);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 1, 6) && h.Name == Epiphany);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 5, 1) && h.Name == LabourDay);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 6, 6) && h.Name == NationalDayOfSweden);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 12, 25) && h.Name == ChristmasDay);
+        holidays.Should().Contain(h => h.Date == new DateOnly(2026, 12, 26) && h.Name == StStephensDay);
+    }
+
+    [Fact]
+    public void GetHolidays_ContainsVariableHolidays()
+    {
+        var easter2026 = EasterSundayCalculation.Christian.ForYear(2026);
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31));
+
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(-2) && h.Name == GoodFriday);
+        holidays.Should().Contain(h => h.Date == easter2026 && h.Name == EasterSunday);
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(1) && h.Name == EasterMonday);
+        holidays.Should().Contain(h => h.Date == easter2026.AddDays(39) && h.Name == AscensionDay);
+    }
+
+    [Fact]
+    public void MidsummerDay_FallsOnSaturdayBetweenJune20And26()
+    {
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31)).ToList();
+
+        var midsummer = holidays.First(h => h.Name == MidsummerDay);
+
+        midsummer.Date.DayOfWeek.Should().Be(DayOfWeek.Saturday);
+        midsummer.Date.Day.Should().BeInRange(20, 26);
+        midsummer.Date.Month.Should().Be(6);
+    }
+
+    [Fact]
+    public void AllSaintsDay_FallsOnSaturdayBetweenOct31AndNov6()
+    {
+        var holidays = NationalHolidays.Create().From(new DateOnly(2026, 1, 1)).To(new DateOnly(2026, 12, 31)).ToList();
+
+        var allSaints = holidays.First(h => h.Name == AllSaintsDay);
+
+        allSaints.Date.DayOfWeek.Should().Be(DayOfWeek.Saturday);
+        (allSaints.Date.Month == 10 && allSaints.Date.Day == 31 || allSaints.Date.Month == 11 && allSaints.Date.Day <= 6)
+            .Should().BeTrue();
+    }
+}
