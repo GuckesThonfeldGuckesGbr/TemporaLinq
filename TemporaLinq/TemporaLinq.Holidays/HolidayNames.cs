f8b3c9e21409e0a4d4ad72277d75ced039cb6287
Christopher Thonfeld-Guckes <ctg@baggerbagger.de>
2026-08-25 10:51:39 +0200

    feat: add HolidayNames values for Tier E1 countries

--- TemporaLinq/TemporaLinq.Holidays/HolidayNames.cs
+++ TemporaLinq/TemporaLinq.Holidays/HolidayNames.cs
@@ -8,40 +8,60 @@ public enum HolidayNames
     AssumptionDay,
     AugsburgPeaceFestival, // Germany
     BastilleDay, // France
+    BattleOfTheBoyneDay, // Northern Ireland
     BirthdayOfMartinLutherKingJr, // USA
     BirthdayOfGeorgeWashington, // USA
+    BoxingDay, // UK, Ireland, Canada, Australia, NZ
     ChristmasDay,
     ColumbusDay, // USA
+    ConstitutionDayOfPoland, // Poland
+    ConstitutionDayOfUkraine, // Ukraine
     CorpusChristi,
     DayOfGermanUnity, // Germany
+    DefendersDay, // Ukraine
+    EarlyMayBankHoliday, // UK
     EasterMonday,
     EasterSunday,
     Epiphany,
     FeastOfStJanuarius, // Italy
     FeastOfStJohnTheBaptist, // Italy
     FeastOfStPetronius, // Italy
+    FlemishCommunityDay, // Belgium
+    FrenchCommunityDay, // Belgium
+    GermanCommunityDay, // Belgium
     GoodFriday,
     ImmaculateConception,
-    IndependenceDay, // USA
+    IndependenceDay, // USA, Ukraine
     InternationalWomensDay,
     Juneteenth, // USA
+    KingsDayOfTheNetherlands, // Netherlands
     LabourDay,
-    LiberationDay, // Italy
+    LiberationDay, // Italy, Netherlands
     MemorialDay, // USA
+    MidsummerDay, // Sweden
+    NationalDayOfAustria, // Austria
+    NationalDayOfBelgium, // Belgium
     NationalDayOfSpain, // Spain
+    NationalDayOfSweden, // Sweden
+    NationalDayOfSwitzerland, // Switzerland
     NewYearsDay,
     ReformationDay, // Germany
     RepentanceAndPrayerDay, // Germany
     RepublicDay, // Italy
     SanMarco, // Italy
+    SecondJanuary, // Scotland
     SpanishConstitutionDay, // Spain
+    SpringBankHoliday, // UK
     StAmbrose, // Italy
+    StAndrewsDay, // Scotland
+    StPatricksDay, // Northern Ireland
     StPeterAndPaul, // Italy
     StStephensDay,
+    SummerBankHoliday, // UK (England, Wales, Northern Ireland)
     ThanksgivingDay, // USA
     WhitMonday,
     WhitSunday,
     WorldChildrensDay,
     VeteransDay, // USA
-    VictoryDay // France
+    VictoryDay // France, Ukraine
 }
