16aedd753ead8f41203ee7c400eed44a9ee1db31
Christopher Thonfeld-Guckes <ctg@baggerbagger.de>
2026-08-25 12:46:29 +0200

    feat: add HolidayNames values for Tier E2 countries

--- TemporaLinq/TemporaLinq.Holidays/HolidayNames.cs
+++ TemporaLinq/TemporaLinq.Holidays/HolidayNames.cs
@@ -7,19 +7,26 @@ public enum HolidayNames
     AscensionDay,
     AssumptionDay,
     AugsburgPeaceFestival, // Germany
+    AugustBankHoliday, // Ireland
     BastilleDay, // France
     BattleOfTheBoyneDay, // Northern Ireland
     BirthdayOfMartinLutherKingJr, // USA
     BirthdayOfGeorgeWashington, // USA
     BoxingDay, // UK, Ireland, Canada, Australia, NZ
+    ChildrensDay, // Romania
     ChristmasDay,
+    ChristmasEve, // Czech Republic
+    CleanMonday, // Greece
     ColumbusDay, // USA
+    ConstitutionDayOfDenmark, // Denmark
+    ConstitutionDayOfNorway, // Norway
     ConstitutionDayOfPoland, // Poland
     ConstitutionDayOfUkraine, // Ukraine
     CorpusChristi,
+    CzechStatehoodDay, // Czech Republic
     DayOfGermanUnity, // Germany
     DefendersDay, // Ukraine
-    EarlyMayBankHoliday, // UK
+    EarlyMayBankHoliday, // UK, Ireland
     EasterMonday,
     EasterSunday,
     Epiphany,
@@ -27,41 +34,56 @@ public enum HolidayNames
     FeastOfStJohnTheBaptist, // Italy
     FeastOfStPetronius, // Italy
     FlemishCommunityDay, // Belgium
+    FreedomDayOfPortugal, // Portugal
     FrenchCommunityDay, // Belgium
     GermanCommunityDay, // Belgium
     GoodFriday,
+    GreekIndependenceDay, // Greece
     ImmaculateConception,
-    IndependenceDay, // USA, Ukraine
+    IndependenceDay, // USA, Ukraine, Finland
+    IndependentCzechoslovakStateDay, // Czech Republic
     InternationalWomensDay,
+    JanHusDay, // Czech Republic
+    JuneBankHoliday, // Ireland
     Juneteenth, // USA
     KingsDayOfTheNetherlands, // Netherlands
     LabourDay,
     LiberationDay, // Italy, Netherlands
+    MaundyThursday, // Denmark, Norway
     MemorialDay, // USA
-    MidsummerDay, // Sweden
+    MidsummerDay, // Sweden, Finland
     NationalDayOfAustria, // Austria
     NationalDayOfBelgium, // Belgium
+    NationalDayOfRomania, // Romania
     NationalDayOfSpain, // Spain
     NationalDayOfSweden, // Sweden
     NationalDayOfSwitzerland, // Switzerland
     NewYearsDay,
+    OctoberBankHoliday, // Ireland
+    OhiDay, // Greece
+    PortugalDay, // Portugal
     ReformationDay, // Germany
     RepentanceAndPrayerDay, // Germany
-    RepublicDay, // Italy
+    RepublicDay, // Italy, Portugal
+    RestorationOfIndependenceDay, // Portugal
+    SaintsCyrilAndMethodiusDay, // Czech Republic
     SanMarco, // Italy
-    SecondJanuary, // Scotland
+    SecondJanuary, // Scotland, Romania
     SpanishConstitutionDay, // Spain
     SpringBankHoliday, // UK
     StAmbrose, // Italy
-    StAndrewsDay, // Scotland
+    StAndrewsDay, // Scotland, Romania
+    StBrigidsDay, // Ireland
     StPatricksDay, // Northern Ireland
     StPeterAndPaul, // Italy
     StStephensDay,
+    StruggleForFreedomAndDemocracyDay, // Czech Republic
     SummerBankHoliday, // UK (England, Wales, Northern Ireland)
+    SynaxisOfStJohnTheBaptist, // Romania
     ThanksgivingDay, // USA
     WhitMonday,
     WhitSunday,
     WorldChildrensDay,
     VeteransDay, // USA
-    VictoryDay // France, Ukraine
+    VictoryDay // France, Ukraine, Czech Republic
 }
